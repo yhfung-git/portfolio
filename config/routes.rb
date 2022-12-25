9c23f178472bfbf32d10712eca64cf1b1ea8e337
yonghowfung <yhfung940217@gmail.com>
2022-12-25 17:26:52 +0100

    added about projects and contact routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
   root to: "pages#home"
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
+  get "about", to: "pages#about"
+  get "projects", to: "pages#projects"
+  get "contact", to: "pages#contact"
 end
+
+# Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+
+# Defines the root path route ("/")
+# root "articles#index"
