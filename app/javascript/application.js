c0d4969e97c9750fe75bd49827a0952b4dfee427
yonghowfung <yhfung940217@gmail.com>
2022-12-27 16:42:11 +0100

    added photo maple, cursor animation to application.js and home.scss

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -2,3 +2,21 @@
 import "@hotwired/turbo-rails"
 import "./controllers"
 import "bootstrap"
+
+document.onmousemove = (e) => {
+  let body = document.querySelector("body");
+  let maple = document.createElement("span");
+  let x = e.pageX;
+  let y = e.pageY;
+  let size = Math.random() * 50;
+
+  maple.style.left = x + "px";
+  maple.style.top = y + "px";
+  maple.style.width = size + "px";
+  maple.style.height = size + "px";
+  body.appendChild(maple);
+
+  setTimeout(() => {
+    maple.remove();
+  }, 2000);
+};
