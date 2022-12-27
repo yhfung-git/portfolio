e331956f760e763b9a9a2d141771f1d2ebca0fca
yonghowfung <yhfung940217@gmail.com>
2022-12-27 14:50:41 +0100

    added page model and migrated

---
+++ db/migrate/20221227134942_create_pages.rb
@@ -0,0 +1,12 @@
+class CreatePages < ActiveRecord::Migration[7.0]
+  def change
+    create_table :pages do |t|
+      t.string :name
+      t.string :email
+      t.string :subject
+      t.text :message
+
+      t.timestamps
+    end
+  end
+end
