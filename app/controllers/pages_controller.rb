1cd7afbba40651ff6e5fbb389057c95131b52645
yonghowfung <yhfung940217@gmail.com>
2022-12-27 15:05:14 +0100

    removed about contact projects from routes views and page controller

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,13 +1,4 @@
 class PagesController < ApplicationController
   def home
   end
-
-  def about
-  end
-
-  def projects
-  end
-
-  def contact
-  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,5 @@
 Rails.application.routes.draw do
   root to: "pages#home"
-  get "about", to: "pages#about"
-  get "projects", to: "pages#projects"
-  get "contact", to: "pages#contact"
 end
 
 # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
