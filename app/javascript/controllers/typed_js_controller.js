5dfbd9fe9db155be23a757f433eb777af8ca7398
yonghowfung <yhfung940217@gmail.com>
2022-12-30 18:35:10 +0100

    changed the typed_js text for top banner

--- app/javascript/controllers/typed_js_controller.js
+++ app/javascript/controllers/typed_js_controller.js
@@ -5,7 +5,7 @@ import Typed from "typed.js"
 export default class extends Controller {
   connect() {
     new Typed(this.element, {
-      strings: ["Click the button below to learn more about me"],
+      strings: ["Click the button below to download my resume"],
       typeSpeed: 30,
       backDelay: 1000,
       loop: true
