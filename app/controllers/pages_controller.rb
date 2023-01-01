69e66eab6d4c24ab02af063558ca327f59d721b9
yonghowfung <yhfung940217@gmail.com>
2023-01-01 21:38:58 +0100

    added download route and downloadable button with CV pdf

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,12 @@
 class PagesController < ApplicationController
   def home
   end
+
+  def download
+    send_file(
+      "#{Rails.root}/app/assets/files/CV_FUNG_Yong_How.pdf",
+      filename: "CV_FUNG_Yong_How.pdf",
+      type: "application/pdf"
+    )
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   root to: "pages#home"
+  get "download", to: "pages#download"
 end
 
 # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
