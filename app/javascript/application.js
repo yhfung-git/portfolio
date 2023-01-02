b7ed86e35ef529c79bea8811371037b9fe9326eb
yonghowfung <yhfung940217@gmail.com>
2023-01-02 21:34:52 +0100

    added timeline css to _about.scss and timeline js animation to application.js

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -3,6 +3,7 @@ import "@hotwired/turbo-rails"
 import "./controllers"
 import "bootstrap"
 
+// Mousemove Animation
 document.onmousemove = (e) => {
   let body = document.querySelector("body");
   let maple = document.createElement("span");
@@ -21,3 +22,31 @@ document.onmousemove = (e) => {
     maple.remove();
   }, 1000);
 };
+
+// Timeline Animation
+var items = document.querySelectorAll(".timeline li");
+
+function isElementInViewport(el) {
+  var rect = el.getBoundingClientRect();
+  return (
+    rect.top >= 0 &&
+    rect.left >= 0 &&
+    rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
+    rect.right <= (window.innerWidth || document.documentElement.clientWidth)
+  );
+}
+
+function callbackFunc() {
+  for (var i = 0; i < items.length; i++) {
+    if (isElementInViewport(items[i])) {
+      if(!items[i].classList.contains("in-view")){
+        items[i].classList.add("in-view");
+      }
+    } else if(items[i].classList.contains("in-view")) {
+        items[i].classList.remove("in-view");
+    }
+  }
+}
+
+window.addEventListener("load", callbackFunc);
+window.addEventListener("scroll", callbackFunc);
