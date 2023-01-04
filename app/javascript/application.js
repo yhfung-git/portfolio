8eeb853b085de96c1c534a5bab19f8e0fa43c6c8
yonghowfung <yhfung940217@gmail.com>
2023-01-04 12:55:25 +0100

    removed scroll line progress bar

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -2,13 +2,3 @@
 import "@hotwired/turbo-rails"
 import "./controllers"
 import "bootstrap"
-
-//capture scroll any percentage
-$(window).scroll(function(){
-var wintop = $(window).scrollTo(), docheight =
-
-    $(document).height(), winheight = $(window).height();
- 			var scrolled = (wintop/(docheight-winheight))*100;
-
-  		$('.scroll-line').scss('width', (scrolled + '%'));
-});
