c050c4ea75b2e648b23fb3bcb05b36595767639e
yonghowfung <yhfung940217@gmail.com>
2023-01-06 21:20:22 +0100

    added toggle function

--- app/javascript/controllers/faq_controller.js
+++ app/javascript/controllers/faq_controller.js
@@ -2,7 +2,32 @@ import { Controller } from "@hotwired/stimulus"
 
 // Connects to data-controller="faq"
 export default class extends Controller {
+  static targets = ["front", "data", "back", "tools", "uix", "video", "softwares"]
+
   connect() {
-    console.log("FAQ connected");
+    // console.log("FAQ connected");
+    // console.log(this.contentTarget);
+  }
+
+  showFront() {
+    this.frontTarget.classList.toggle("hidden")
+  }
+  showData() {
+    this.dataTarget.classList.toggle("hidden")
+  }
+  showBack() {
+    this.backTarget.classList.toggle("hidden")
+  }
+  showTools() {
+    this.toolsTarget.classList.toggle("hidden")
+  }
+  showUix() {
+    this.uixTarget.classList.toggle("hidden")
+  }
+  showVideo() {
+    this.videoTarget.classList.toggle("hidden")
+  }
+  showSoftwares() {
+    this.softwaresTarget.classList.toggle("hidden")
   }
 }
